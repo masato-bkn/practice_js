10a27052fa553850d572ea47bb229816be79aef4
重田 将人 <masato.0928.j@gmail.com>
2021-01-13 09:11:42 +0900

    promise
    - rejectedなPromiseインスタンスを生成
    - promise チェーン
    - promise.all

--- async.js
+++ async.js
@@ -117,7 +117,7 @@ dummyFetch("/success/data").then(
 // =======================================================
 
 // =======================================================
-// primiseの状態
+// promiseの状態
 //  - fullfilled
 //  - rejected -> 成功時メソッドが呼ばれる
 //  - pending -> 失敗時メソッドが呼ばれる
@@ -137,4 +137,81 @@ promise.then(() => {
 });
 console.log("1. 同期的な処理が実行されました")
 
+// =======================================================
+
+// =======================================================
+// rejectedなPromiseインスタンスを生成
+// Promise.rejectメソッドはnew Promiseの糖衣構文
+var rejectedPromise = Promise.reject(new Error("エラー"));
+
+var rejectedPromise = new Promise(
+    (resolve, reject) => {
+        reject(new Error("エラー"));
+    }
+);
+
+Promise.reject(new Error("エラー")).catch(()=> {
+    console.log("2.コールバック関数が実行されました")
+});
+
+console.log("1. 同期的な処理が実行されました")
+// =======================================================
+
+// =======================================================
+// promise チェーン
+function asyncTask() {
+    return Math.random() > 0.5
+    ? Promise.resolve("成功")
+    : Promise.reject(new Error("失敗"));
+}
+
+
+// asyncTask関数は新しい`Promise`インスタンスを返す
+asyncTask()
+    // thenメソッドは新しい`Promise`インスタンスを返す
+    .then(
+        function onFulfilled(value) {
+            console.log(value); // => 成功
+        }
+    )
+    // thenメソッドは新しい`Promise`インスタンスを返す
+    .catch(
+        function onRejected(error) {
+            console.log(error.message); // => 失敗
+        }
+    )
+// 成功したらthenが実行される
+// 失敗したらthenは無視されてcatchが実行される
+// **thenやcatchメソッドはFullfilled状態のPromiseインスタンを返す**
+// =======================================================
+
+// =======================================================
+// promise.all
+
+function dummyFetch(path) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (path.startsWith("/resource")) {
+                resolve({body: `Response body of ${path}`});
+            } else {
+                reject(new Error("NOT FOUND"));
+            }
+        },
+        100 * Math * random());
+    })
+}
+
+var fetchedPromse = Promise.all([
+    dummyFetch("/resource/A"),
+    dummyFetch("/resource/B")
+]);
+
+fetchedPromise.then(([responseA, responseB]) => {
+    // この行は実行されません
+}).catch(error => {
+    console.error(error); // Error: NOT FOUND
+});
+
+// すべて成功したらthen、一つでも失敗したらcatchが呼び出される
+// そういう意味でall
 // =======================================================
