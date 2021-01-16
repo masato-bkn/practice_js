b8c366a939e8cf677dc004f7ce31c1ee2d64da51
重田 将人 <masato.0928.j@gmail.com>
2021-01-16 10:31:00 +0900

    マークダウンをhtml形式に変換

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,6 +1,26 @@
 const program = require("commander");
+const fs = require("fs");
+const marked = require("marked");
 
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
-
 const filePath = program.args[0];
-console.log(filePath);
+
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+    gfm: false,
+    ...program.opts(),
+};
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        process.exit(1);
+        return;
+    }
+    // gfmオプションを無効にする
+    const html = marked(file, {
+        gfm: false
+    });
+    console.log(html);
+});
